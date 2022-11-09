3c5caa38158364c9724b11a978557385e1bf34db
waroad <78641034+waroad@users.noreply.github.com>
2022-11-09 22:46:54 +0900

    Update jsonFormating.py

--- jsonFormating.py
+++ jsonFormating.py
@@ -7,8 +7,6 @@ with open("settings.json", "r") as json_file:
     for k in dict1['path']:
         path_sum_mdpo+=k+' '
         path_sum_sync+='^'+k+'|'
-    print(path_sum_sync[:-1])
-    print(path_sum_mdpo[:-1])
     dict1["path_sum_sync"] = path_sum_sync[:-1]
     dict1["path_sum_mdpo"] = path_sum_mdpo[:-1]
     del dict1["path"]
